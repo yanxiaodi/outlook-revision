9fb85a9ff11df5854ce07b7360ff4fb91fdc12f9
yanxiaodi <yan_xiaodi@hotmail.com>
2024-05-13 22:44:06 +1200

    Add a new controller

---
+++ src/api/FunCoding.ReVision/FunCoding.ReVision.WebApi/Controllers/OutlookController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunCoding.ReVision.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OutlookController : ControllerBase
+{
+}
