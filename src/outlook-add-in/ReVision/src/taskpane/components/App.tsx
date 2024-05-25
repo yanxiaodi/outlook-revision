3c59bf055dcae394a6fee617e7037d8769fa4fee
yanxiaodi <yan_xiaodi@hotmail.com>
2024-05-25 22:43:46 +1200

    Remove useless files

--- src/outlook-add-in/ReVision/src/taskpane/components/App.tsx
+++ src/outlook-add-in/ReVision/src/taskpane/components/App.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
-import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
-import TextInsertion from "./TextInsertion";
 import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
-import { insertText } from "../taskpane";
+
 import Settings, { Language, SettingsState } from "./Settings";
 import TabsContainer from "./TabsContainer";
 
@@ -32,22 +28,6 @@ export const SettingsContext = React.createContext<{
 
 const App: React.FC<AppProps> = (_props: AppProps) => {
   const styles = useStyles();
-  // The list items are static and won't change at runtime,
-  // so this should be an ordinary const, not a part of state.
-  const listItems: HeroListItem[] = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
 
   // Create a language list
   const languageList: Language[] = [
